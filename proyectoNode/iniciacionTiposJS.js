cbcbb2e8c4718a96f27411ef10a167fcb3fd55e4
Diego Franco Ponce Jordanes <62223849+franponce@users.noreply.github.com>
2022-06-21 13:42:44 -0300

    Add files via upload

---
+++ proyectoNode/iniciacionTiposJS.js
@@ -0,0 +1,32 @@
+// Tipos primitivos
+
+// number 
+
+4
+
+// string 
+
+"Hola mundo";
+'Hola mundo';
+`Hola mundo`;
+
+// booleanos 
+
+true;
+false;
+
+// nulo y undefined
+
+null;
+undefined;
+
+// null, undefined, false, 0 ===> Todas son False
+console.log(null === undefined);
+
+if (null) {
+    console.log("cumple");
+} else {
+    console.log("no cumple");
+}
+
+
